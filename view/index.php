517e1c133b95f68d33664d4b6e0db1a18d957efb
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-09-09 11:37:55 +0700

    msg-add: add navbar

---
+++ view/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;900&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="images/logo_kedaiJawir.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles/home.css">
+    <title> Selamat Datang di KedaiJawir </title>
+</head>
+
+<body>
+    <div class="container">
+
+    <!-- CONTAINER UNTUK ATUR NAVBAR -->
+        <div class="container_navbar">
+            <div class="wrapper_navbar">
+                <div class="wrapper_logoImage">
+                    <!-- <img src="images/logo_kedaiJawir.png" alt="logo KedaiJawir" width="10%" height="10%"> -->
+                    KedaiJawir
+                </div>
+                <div class="wrapper_navMenu">
+                    <ul style="list-style: none;">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About us</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Order</a></li>
+                    </ul>
+                </div>
+                <div class="wrapper_navAuth">
+                    Login
+                    Register
+                </div>
+            </div>
+        </div>
+
+
+    <!-- CONTAINER UNTUK ATUR BAGIAN CONTENT -->
+        <div class="container_content">
+            ini content
+        </div>
+
+
+    <!-- CONTAINER UNTUK ATUR BAGIAN FOOTER-->
+
+        <div class="container_footer">
+        </div>
+    </div>
+</body>
+
+</html>
