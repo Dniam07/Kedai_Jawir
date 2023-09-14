1ce58dd3ae1de037fdd2a0f2cda48fedb7e6f9fa
Dniam07 <gamingopan123@gmail.com>
2023-09-14 18:17:56 +0700

    msg-upd: add product crud and some style.

--- controller/admin/products.php
+++ controller/admin/products.php
@@ -1,5 +1,5 @@
 <?php
-require '../includes/functions.php';
+require '../includes/actions.php';
 
 
 // SHOW DATAS //
@@ -7,7 +7,7 @@ $result = fetchProduct();
 //--------------//
 
 // increment number //
-    $i = 1;
+$i = 1;
 //--------------//
 ?>
 
@@ -22,32 +22,94 @@ $result = fetchProduct();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SHOW PRODUCTS</title>
     <link rel="shortcut icon" href="../../images/img-jawir.png" type="image/x-icon">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="wrapper">
             <div class="content">
-                <!-- SHOW DATA -->
-                <table border="1">
-                    <tr>
-                        <th>No </th>
-                        <th>Product Name </th>
-                        <th>Price </th>
-                    </tr>
+                <div class="content-table">
 
-                    <?php foreach ($result as $row) : ?>
+                    <!-- SHOW DATA -->
+                    <table border="1">
                         <tr>
-                            <td><?= $i++ ?></td>
-                            <td>
-                                <?php echo $row['product_name'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['price'] ?>
-                            </td>
+                            <th>No </th>
+                            <th>Product Name </th>
+                            <th>Price </th>
+                            <th>Action</th>
                         </tr>
+
+                        <?php foreach ($result as $row) : ?>
+                            <tr>
+                                <td><?= $i++ ?></td>
+                                <td>
+                                    <?php echo $row['product_name'] ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['price'] ?>
+                                </td>
+                                <td>
+                                <td>
+                                    <div class="action-buttons">
+                                        <a href="?delete_id=<?php echo $row['product_id'] ?>" class="action-button delete-button" onclick="return confirm('ARE YOU SURE?')">Delete</a>
+                                        <a href="?edit_id=<?php echo $row['product_id'] ?>" class="action-button edit-button" onclick="return confirm('ARE YOU SURE?')">Edit</a>
+                                    </div>
+                                </td>
+
+
+                                </td>
+                            </tr>
                         <?php endforeach; ?>
+
                     </table>
+                </div>
+
+                <div class="content-addForm">
+
+                    <form action="" method="post">
+                        <input type="hidden" name="product_id" id="product_id">
+                        <ul class="">
+
+                            <li>
+                                <input type="text" name="productName" id="productName" autocomplete="off" placeholder="Insert Product Name" required>
+                            </li>
+
+                            <li>
+                                <input type="text" name="price" id="price" autocomplete="off" placeholder="Insert Product Price" required>
+                            </li>
+
+                        </ul>
+                        <button type="submit" name="submitProduct" class="submit-form">ADD</button>
+
+                    </form>
+
+                </div>
+
+                <div class="content-editForm">
+
+                    <form action="" method="post">
+                        <ul class="form-mobil">
+                            <li>
+                                <input type="text" name="edit_id" id="edit_id" value="<?= $getId['product_id'] ?>">
+                            </li>
+                            <li>
+                                <input type="text" name="productName" id="productName" autocomplete="off" value="<?= $getId['product_name']; ?>" required>
+                            </li>
+
+                            <li>
+                                <input type="text" name="price" id="price" autocomplete="off" value="<?= $getId['price']; ?>" required>
+                            </li>
+
+                        </ul>
+                        <button type="submit" name="editProduct" class="edit-form">Edit</button>
+
+                    </form>
+
+
+                </div>
+
+
             </div>
         </div>
     </div>
--- controller/includes/functions.php
+++ controller/includes/functions.php
@@ -15,17 +15,6 @@ function getDatas($query)
     return $rows;
 }
 
-function fetchProduct()
-{
-    global $getConnect;
-    $query = "SELECT * FROM products";
-    $result = mysqli_query($getConnect, $query);
-    $rows = [];
-    while ($row = mysqli_fetch_assoc($result)) {
-        $rows[] = $row;
-    }
-    return $rows;
-}
 function fetchCustomer()
 {
     global $getConnect;
@@ -52,6 +41,19 @@ function fetchTransaction()
             INNER JOIN products ON products.product_id = transactions.product_id
             ORDER BY transactions.transaction_id DESC";
 
+$result = mysqli_query($getConnect, $query);
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+}
+return $rows;
+}
+
+// FUNCTION CRUD PRODUCT //
+function fetchProduct()
+{
+    global $getConnect;
+    $query = "SELECT * FROM products";
     $result = mysqli_query($getConnect, $query);
     $rows = [];
     while ($row = mysqli_fetch_assoc($result)) {
@@ -59,8 +61,39 @@ function fetchTransaction()
     }
     return $rows;
 }
+function addProduct()
+{
+    global $getConnect;
+    $productName = htmlspecialchars($_POST['productName']);
+    $price = htmlspecialchars($_POST['price']);
+  
+    $query = "INSERT INTO products VALUES (null,'$productName', '$price')";
+    $result = mysqli_query($getConnect, $query);
+    return $result;
+}
+
+function deleteProduct($productDelete_id)
+{
+    global $getConnect;
+    $query = "DELETE FROM products WHERE product_id= '$productDelete_id'";
+    $result = mysqli_query($getConnect, $query);
+    return mysqli_affected_rows($getConnect);
+}
+function updateProduct(){
+    global $getConnect;
+    $productName = htmlspecialchars($_POST['productName']);
+    $price = htmlspecialchars($_POST['price']);
+    $edit_id = $_POST['edit_id'];
+
+    $query = "UPDATE products SET
+          product_name = '$productName',
+          price = '$price'
+          WHERE product_id = $edit_id";
 
 
-
+    $result = mysqli_query($getConnect, $query);
+    return $result;
+}
+// ============================================= //
 
 ?>
